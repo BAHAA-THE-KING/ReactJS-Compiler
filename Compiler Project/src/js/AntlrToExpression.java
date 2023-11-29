b5ba8752d6e3f94442bf372484904cc5d0626ad5
AbdullahRahmoun1 <aboodyggkol@gmail.com>
2023-11-29 17:50:04 +0300

    added memberIndexExpression models and visitors

--- Compiler Project/src/js/AntlrToExpression.java
+++ Compiler Project/src/js/AntlrToExpression.java
@@ -3,6 +3,7 @@ package js;
 import antlrJS.JSParser;
 import antlrJS.JSParserBaseVisitor;
 import js.ClassDeclaration.ClassDeclaration;
+import js.ImportStatement.MemberIndex;
 
 public class AntlrToExpression extends JSParserBaseVisitor<Expression> {
     @Override
@@ -10,5 +11,13 @@ public class AntlrToExpression extends JSParserBaseVisitor<Expression> {
         String name = ctx.Identifier() != null ? ctx.Identifier().getText() : "";
         ClassDeclaration classDeclaration = new ClassDeclaration(name, "");
         return classDeclaration;
+    }
+
+    @Override
+    public Expression visitMemberIndexExpression(JSParser.MemberIndexExpressionContext ctx) {
+        Expression accessedExpression = this.visit(ctx.singleExpression());
+        ExpressionSequence accessedAt = new ExpressionSequence(ctx.expressionSequence());
+        return new MemberIndex(accessedExpression,accessedAt);
+
     }
 }

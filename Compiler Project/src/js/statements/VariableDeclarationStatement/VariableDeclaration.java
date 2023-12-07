b78517bb8fe5e9e51183e027955687e8e2df5839
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2023-12-07 14:47:28 +0300

    Add Modifier To Identifier

--- Compiler Project/src/js/statements/VariableDeclarationStatement/VariableDeclaration.java
+++ Compiler Project/src/js/statements/VariableDeclarationStatement/VariableDeclaration.java
@@ -5,18 +5,15 @@ import js.visitors.models.Assignable;
 import js.visitors.models.Expression;
 
 public class VariableDeclaration implements Symbol {
-    Assignable name;
-    Expression value;
-    String type;
+    public String modifier;
+    public Assignable name;
+    public Expression value;
+    public String type;
 
-    public VariableDeclaration(Assignable name, Expression value, String type) {
+    public VariableDeclaration(String modifier, Assignable name, Expression value, String type) {
+        this.modifier = modifier;
         this.name = name;
         this.value = value;
         this.type = type;
-    }
-
-    @Override
-    public String toString() {
-        return "VariableDeclaration{" + "name=" + name + ", value=" + value + '}';
     }
 }

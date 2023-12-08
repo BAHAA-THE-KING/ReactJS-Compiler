a53b9eaa9a763a181da179efbecc62675bb70992
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2023-12-08 17:12:15 +0300

    Remove Test Code From Main

--- Compiler Project/src/Main.java
+++ Compiler Project/src/Main.java
@@ -1,20 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String args[]) {
-        System.out.println(asd.class.getSimpleName());
-        asd dsa=new asd("koko", new ArrayList<>());
-
-        System.out.println();
-
-    }
-}
-class asd{
-    String name;
-    List<Integer> nums;
-    public asd(String name,List<Integer> nums){
-        this.name=name;
-        this.nums=nums;
     }
 }

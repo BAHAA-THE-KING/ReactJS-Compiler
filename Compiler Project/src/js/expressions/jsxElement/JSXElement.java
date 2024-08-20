8b07e582d8d527b1a061e34d06ec99a8f020e057
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2024-08-21 02:36:16 +0300

    Use JSX Converter.

--- Compiler Project/src/js/expressions/jsxElement/JSXElement.java
+++ Compiler Project/src/js/expressions/jsxElement/JSXElement.java
@@ -4,9 +4,9 @@ import js.visitors.models.AttributeValue;
 import js.visitors.models.Expression;
 import js.visitors.models.JSXContent;
 import org.antlr.v4.runtime.misc.Pair;
+import program.JSXConverter;
 
 import java.util.List;
-import java.util.StringJoiner;
 
 public class JSXElement implements Expression, JSXContent {
     public String tagName;
@@ -21,13 +21,14 @@ public class JSXElement implements Expression, JSXContent {
 
     @Override
     public String toString() {
-        StringJoiner attributesJoiner = new StringJoiner(" ");
-        attributes.forEach(att -> attributesJoiner.add(att.a + "=" + att.b));
-
-        StringJoiner bodyJoiner = new StringJoiner("\n");
-        body.forEach(cnt -> bodyJoiner.add(cnt.toString()));
-
-        if (body.size() == 0) return "<" + tagName + " " + attributesJoiner + " />";
-        return "<" + tagName + " " + attributesJoiner + ">\n" + bodyJoiner + "</" + tagName + ">";
+        return JSXConverter.JsxToFunction(this).toString();
+//        StringJoiner attributesJoiner = new StringJoiner(" ");
+//        attributes.forEach(att -> attributesJoiner.add(att.a + "=" + att.b));
+//
+//        StringJoiner bodyJoiner = new StringJoiner("\n");
+//        body.forEach(cnt -> bodyJoiner.add(cnt.toString()));
+//
+//        if (body.size() == 0) return "<" + tagName + " " + attributesJoiner + " />";
+//        return "<" + tagName + " " + attributesJoiner + ">\n" + bodyJoiner + "</" + tagName + ">";
     }
 }
